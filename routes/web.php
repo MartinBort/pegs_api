96ee68a7dd49f2863d76127bbc048186cdc68fb1
Martin Bortagaray <Borts@martins-air.home>
2017-05-01 15:28:15 +0100

    added Laravel authentication

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,18 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('users', ['uses' => 'UsersController@index']);
 Route::get('users/create', ['uses' => 'UsersController@create']);
 Route::post('users', ['uses' => 'UsersController@store']);
+Auth::routes();
+
+
